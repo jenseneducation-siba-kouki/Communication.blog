1be6a16c4b0eb9f17999c28b52ef0e1b7a2f38dd
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-10-19 14:01:37 +0200

    edit the sign in/up

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const navRoutes = require("./routes/nav");
 
 
 app.set("view engine", "ejs")
 
-
+app.use(express.static(path.join(__dirname, "/public")));
 app.use( express.urlencoded({extended: true}) )
-app.use(express.static('public'))
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req,res) => {
-    res.render("index")
-})
-
-app.get('/register', (req,res) => {
-    res.render("register")
-})
+app.use("/", navRoutes);
 
 
 app.listen(8000, () => console.log("Server started"))
---
+++ routes/nav.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => res.render("pages/index"));
+router.get("/signup", (req, res) => res.render("pages/signup"));
+router.get("/login", (req, res) => res.render("pages/login"));
+
+
+module.exports = router;
