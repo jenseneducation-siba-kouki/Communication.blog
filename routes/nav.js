f9d96b7562a426697c68b713667c6b5896358d12
jenseneducation-siba-kouki <54539086+jenseneducation-siba-kouki@users.noreply.github.com>
2020-10-23 21:18:19 +0200

    Create blog

--- routes/nav.js
+++ routes/nav.js
@@ -9,6 +9,8 @@ router.get("/login", (req, res) => res.render("pages/login"));
 router.get("/dashboard", auth, (req, res) =>
   res.render("pages/dashboard", { user: req.user })
 );
-
+router.get("/create", auth, (req, res) =>
+  res.render("pages/create", { user: req.user })
+);
 
 module.exports = router;
